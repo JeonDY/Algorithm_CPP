cb919efb6432569002ac81e19d07e41f6d5c536e
JeonDY <jeondayoon27@gmail.com>
2024-07-23 13:30:33 +0900

    dfs

---
+++ DAYUN/240716/18809.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int n;
+int m;
+int g;
+int r;
+int map[50][50];
+int yellow[10][2];
+int red[5];
+int green[5];
+int spreadMap[50][50];
+int cntYellow;
+
+void init();
+void initRG();
+int chooseRG();
+
+int main(){
+    init();
+
+    //choose 
+    int i = 0;
+    initRG();
+    //Choose Red and Green
+
+    
+
+}
+
+void initRG(){
+    //memset(green, 0, sizeof(green));
+    // memset(red, 0, sizeof(red));
+
+    int cnt = 0;
+    for(int i = 0; i < g; ++i){
+        green[i] = i;
+    }
+    for(int i = 0; i < r; ++i) {
+        red[i] = i + g;
+    }
+    return;
+}
+
+bool chooseRG(){
+    //Choose 
+}
+
+void init(){
+    cntYellow = 0;
+
+    cin >> n >> m >> g >> r;
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < m; ++j) {
+            cin >> map[i][j];
+            if(map[i][j] == 2) {
+                yellow[cntYellow++][0] = i;
+                yellow[cntYellow++][0] = j;
+            }
+        }
+    }
+
+
+}
