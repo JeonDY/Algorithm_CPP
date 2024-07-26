06f4fbef6e34c40b359591ef77273b09cff44fd8
JeonDY <jeondayoon27@gmail.com>
2024-07-26 16:43:31 +0900

    [BOJ] 17069 G5

--- DAYUN/240723/[BOJ] 17070 G5.cpp
+++ DAYUN/240723/[BOJ] 17070 G5.cpp
@@ -30,30 +30,34 @@ int find(){
     //첫 좌표에 넣지 못하는 경우
     if(map[0][0] == 1 || map[0][1] == 1) return 0;
     installedPipe[0][1][0] = 1;
-    //s : sum  s = i + j
-    for(int s = 0; s < ((n - 1) * 2); ++s) {
-        for(int j = 0; j <= s;  ++j) {
-            int i = s - j;
-            if(i >= n || j >= n) continue;
+
+    for(int i = 0; i < n; ++i) {
+        for(int j = 1; j < n; ++j) {
             for(int degree = 0; degree < 3; ++degree) {
                 if(installedPipe[i][j][degree] == 0) continue;
                 for(vector<int>::iterator it = nextPipe[degree].begin(); it != nextPipe[degree].end(); ++it) {
                     int nextI = i + nextDir[*it].first;
                     int nextJ = j + nextDir[*it].second;
-                    if(nextI > n || nextJ > n) continue;
+                    if(nextI >= n || nextJ >= n) continue;
                     if(*it == 1) {
                         if(map[nextI - 1][nextJ] == 1) continue;
                         if(map[nextI][nextJ - 1] == 1) continue;
                     }
                     if(map[nextI][nextJ] == 1) continue;
-                    installedPipe[nextI][nextJ][*it]++;
+                    installedPipe[nextI][nextJ][*it] += installedPipe[i][j][degree];
                 }
             }
         }
     }
 
+    // for(int i = 0; i < n; ++i) {
+    //     for(int j = 0; j < n; ++j) {
+    //         cout << installedPipe[i][j][0] + installedPipe[i][j][1] + installedPipe[i][j][2] << " ";
+    //     }
+    //     cout << "\n";
+    // }
     return installedPipe[n - 1][n - 1][0] + installedPipe[n - 1][n - 1][1] + installedPipe[n - 1][n - 1][2]; 
-    
+
 }
 
 int main(){
